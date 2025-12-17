3cbef9ceb688a77a66de45cc96418398eea1b9cd
Enesb06 <111968840+Enesb06@users.noreply.github.com>
2025-12-17 17:32:52 +0300

    update supabase key and url

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // config.php
 
 
-$supabaseUrl = 'SUPABASE_URL_ADRESIN';
-$supabaseAnonKey = 'SUPABASE_ANON_KEY_DEGERIN';
+$supabaseUrl = 'https://infmglbngspopnxrjnfv.supabase.co';
+$supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImluZm1nbGJuZ3Nwb3BueHJqbmZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5NzM5MjQsImV4cCI6MjA4MTU0OTkyNH0.GBzIbFNM6ezxtDAlWpCIMSabhTmXAXOALNrVkpEgS2c';
 
 ?>
